23b058cebc7b306a197111a90ee87d8fe29b6bb5
Vadzim Zianiuk <Vadzim_Zianiuk@epam.com>
2021-03-29 13:17:48 +0300

    refactor

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -14,26 +14,12 @@ namespace Task1
 
         public double Price { get; }
 
-        public bool Equals(Product other)
-        {
-            if (other is null)
-            {
-                return false;
-            }
-
-            return string.CompareOrdinal(this.Name, other.Name) == 0 && this.Price == other.Price;
-        }
+        public bool Equals(Product other) => other != null && 
+            this.Name == other.Name && 
+            this.Price == other.Price;
 
         ///<inheritdoc/>
-        public override bool Equals(object obj)
-        {
-            if (obj is Product product)
-            {
-                return this.Equals(product);
-            }
-
-            return false;
-        }
+        public override bool Equals(object obj) => this.Equals(obj as Product);
 
         ///<inheritdoc/>
         public override int GetHashCode() => HashCode.Combine(this.Name, this.Price);
